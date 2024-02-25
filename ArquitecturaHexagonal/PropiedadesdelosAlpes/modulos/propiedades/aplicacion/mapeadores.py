68c2f929a866e85f27e23d89373fa235a605ea14
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-02-25 13:30:10 -0500

    new branch

---
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/aplicacion/mapeadores.py
@@ -0,0 +1,4 @@
+from PropiedadesdelosAlpes.seedwork.aplicacion.dto import AppMap
+from PropiedadesdelosAlpes.seedwork.dominio.repositorios import Mapeador as RepMap
+from PropiedadesdelosAlpes.modulos.propiedades.dominio.entidades import Locacion
+#from PropiedadesdelosAlpes.modulos.propiedades.dominio.objetos_valor import 
