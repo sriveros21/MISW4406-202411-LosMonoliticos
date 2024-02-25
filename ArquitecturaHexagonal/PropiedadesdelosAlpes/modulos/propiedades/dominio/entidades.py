41196886c709b6eaea1e9bc39feb5f18648e2832
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-02-24 19:07:00 -0500

    Add entidades/dominio

---
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/dominio/entidades.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+import ArquitecturaHexagonal.PropiedadesdelosAlpes.modulos.propiedades.objetos_valor as ov
+from ArquitecturaHexagonal.PropiedadesdelosAlpes.seedwork.dominio.entidades import Entidad
+
+
+@dataclass
+class Tamano(Entidad):
+
+
+@dataclass
+class Fotografias(Entidad):
+
+
+@dataclass
+class Zonificacion(Entidad):
+
+
+@dataclass
+class Estacionamiento(Entidad):
