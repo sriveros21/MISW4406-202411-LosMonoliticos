fa8f8a47680d00c04167032567cb2c23ef5e6ee8
Carlos Tovar <c.tovarb@uniandes.edu.co>
2024-02-25 13:00:25 -0500

    feat: adding infrastructure

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/infraestructura/fabricas.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/infraestructura/fabricas.py
@@ -8,16 +8,14 @@ objetos complejos en la capa de infraestructura del dominio de propiedades
 from dataclasses import dataclass, field
 from PropiedadesdelosAlpes.seedwork.dominio.fabricas import Fabrica
 from PropiedadesdelosAlpes.seedwork.dominio.repositorios import Repositorio
-from PropiedadesdelosAlpes.modulos.propiedades.dominio.repositorios import RepositorioClientes, RepositorioPropiedades
-from .repositorios import RepositorioPropiedadesSQLite, RepositorioClientesSQLite
+from PropiedadesdelosAlpes.modulos.propiedades.dominio.repositorios import RepositorioPropiedades
+from .repositorios import RepositorioPropiedadesSQLite
 from .excepciones import ExcepcionFabrica
 
 @dataclass
 class FabricaRepositorio(Fabrica):
     def crear_objeto(self, obj: type, mapeador: any = None) -> Repositorio:
-        if obj == RepositorioClientes.__class__:
-            return RepositorioClientesSQLite()
-        elif obj == RepositorioPropiedades.__class__:
+        if obj == RepositorioPropiedades.__class__:
             return RepositorioPropiedadesSQLite()
         else:
             raise ExcepcionFabrica()
