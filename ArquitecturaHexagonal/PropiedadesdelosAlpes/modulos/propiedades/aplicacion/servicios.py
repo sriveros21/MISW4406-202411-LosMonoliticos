a73c557e1b2d8eb18fa5e4605abc93b3e084fc07
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-02-25 14:43:00 -0500

    Aplicationbranch updated

---
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/aplicacion/servicios.py
@@ -0,0 +1,35 @@
+from PropiedadesdelosAlpes.seedwork.aplicacion.servicios import Servicio
+from PropiedadesdelosAlpes.modulos.propiedades.dominio.entidades import Propiedad
+from PropiedadesdelosAlpes.modulos.propiedades.dominio.fabricas import FabricaPropiedades
+from PropiedadesdelosAlpes.modulos.propiedades.infraestructura.fabricas import FabricaRepositorio
+from PropiedadesdelosAlpes.modulos.propiedades.infraestructura.repositorios import RepositorioPropiedades
+from .mapeadores import MapeadorPropiedad
+
+from .dto import PropiedadDTO
+
+class ServicioReserva(Servicio):
+
+    def __init__(self):
+        self._fabrica_repositorio: FabricaRepositorio = FabricaRepositorio()
+        self._fabrica_propiedades: FabricaPropiedades = FabricaPropiedades()
+
+    @property
+    def fabrica_repositorio(self):
+        return self._fabrica_repositorio
+    
+    @property
+    def fabrica_propiedades(self):
+        return self._fabrica_propiedades
+
+    def crear_propiedad(self, propiedad_dto: PropiedadDTO) -> PropiedadDTO:
+        propiedad: Propiedad = self.fabrica_propiedades.crear_objeto(propiedad_dto, MapeadorPropiedad())
+
+        repositorio = self.fabrica_repositorio.crear_objeto(RepositorioPropiedades.__class__)
+        repositorio.agregar(propiedad)
+
+        return self.fabrica_propiedades.crear_objeto(propiedad, MapeadorPropiedad())
+
+    def obtener_propiedad_por_id(self, id) -> PropiedadDTO:
+        repositorio = self.fabrica_repositorio.crear_objeto(RepositorioPropiedades.__class__)
+        return repositorio.obtener_por_id(id).__dict__
+
