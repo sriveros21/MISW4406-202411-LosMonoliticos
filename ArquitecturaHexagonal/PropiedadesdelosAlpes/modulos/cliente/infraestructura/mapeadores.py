3bbd59ee45d6ee7e36ea5a03e722e9178f25d5a2
Carlos Tovar <c.tovarb@uniandes.edu.co>
2024-02-25 19:31:15 -0500

    feat: adding infrastructure clientes

---
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/cliente/infraestructura/mapeadores.py
@@ -0,0 +1,36 @@
+""" Mapeadores para la capa de infrastructura del dominio de propiedades
+
+En este archivo usted encontrará los diferentes mapeadores
+encargados de la transformación entre formatos de dominio y DTOs
+
+"""
+
+from PropiedadesdelosAlpes.seedwork.dominio.repositorios import Mapeador
+from PropiedadesdelosAlpes.modulos.cliente.dominio.entidades import Cliente
+from .dto import Cliente as ClienteDTO
+from typing import List
+
+class MapeadorClientes(Mapeador):
+    _FORMATO_FECHA = '%Y-%m-%dT%H:%M:%SZ'
+    
+    def obtener_tipo(self) -> type:
+        return Cliente.__class__
+  
+    def entidad_a_dto(self, entidad: Cliente) -> ClienteDTO:
+
+        cliente_dto = ClienteDTO()
+        cliente_dto.id_cliente = entidad.id
+        cliente_dto.nombre_cliente = entidad.nombre
+        cliente_dto.email_cliente = entidad.email
+
+        return cliente_dto
+
+    
+    def dto_a_entidad(self, dto: ClienteDTO) -> Cliente:
+        cliente = Cliente(
+            dto.id_cliente,
+            dto.nombre_cliente,
+            dto.email_cliente
+        )
+        
+        return cliente
