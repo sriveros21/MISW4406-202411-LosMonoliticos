98a131db71ac052ab02ca8727876370676b7e0a4
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-02-27 17:36:32 -0500

    Adding Domain Events

---
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/auditorias/dominio/eventos.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from PropiedadesdelosAlpes.seedwork.dominio.eventos import (EventoDominio)
+from datetime import datetime
+
+@dataclass
+class AuditoriaCreada(EventoDominio):
+    codigo: str = None
+    fecha_creacion: datetime = None
+
+#Revisar sí aca la fase debe ponerse con Enum
+@dataclass
+class FaseAuditoriaCambiada(EventoDominio):
+    codigo: str = None
+    nueva_fase: str = None
+    fecha_actualizacion: datetime = None
+
--- ArquitecturaHexagonal/PropiedadesdelosAlpes/seedwork/dominio/entidades.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/seedwork/dominio/entidades.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass, field
+from .eventos import EventoDominio
 from .mixins import ValidarReglasMixin
 from .reglas import IdEntidadEsInmutable
 from .excepciones import IdDebeSerInmutableExcepcion
 from datetime import datetime
 import uuid
+
+
 
 @dataclass
 class Entidad:
@@ -17,7 +20,13 @@ class Entidad:
 
 @dataclass
 class AgregacionRaiz(Entidad, ValidarReglasMixin):
-    pass
+    eventos: list[EventoDominio] = field(default_factory=list)
+
+    def agregar_evento(self, evento: EventoDominio):
+        self.eventos.append(evento)
+    
+    def limpiar_eventos(self):
+        self.eventos = list()
 
 @dataclass
 class Locacion(Entidad):
