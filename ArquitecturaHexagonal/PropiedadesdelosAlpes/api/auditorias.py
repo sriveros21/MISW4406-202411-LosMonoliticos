0d212bf1342abf0b2a59611c2bae6b7e6e4fdfda
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-02-27 10:40:08 -0500

    working on auditorias api

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/api/auditorias.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/api/auditorias.py
@@ -4,32 +4,32 @@ from ..modulos.propiedades.aplicacion.dto import PropiedadDTO
 from ..seedwork.dominio.excepciones import ExcepcionDominio
 from ..modulos.auditorias.aplicacion.mapeadores import MapeadorAuditoriaDTOJson
 
-bp = Blueprint('propiedades', __name__, url_prefix='/propiedades')
+bp = Blueprint('auditorias', __name__, url_prefix='/auditorias')
 
-@bp.route('/propiedad', methods=('POST',))
-def crear_propiedad():
+@bp.route('/auditoria', methods=('POST',))
+def crear_auditoria():
     try:
-        propiedad_dict = request.json
+        auditoria_dict = request.json
 
-        map_propiedad = MapeadorPropiedadDTOJson()
-        propiedad_dto = map_propiedad.externo_a_dto(propiedad_dict)
+        map_auditoria = MapeadorAuditoriaDTOJson()
+        auditoria_dto = map_auditoria.externo_a_dto(auditoria_dict)
 
-        sp = ServicioPropiedad()
-        dto_final = sp.crear_propiedad(propiedad_dto)
+        sp = ServicioAuditoria()
+        dto_final = sp.crear_auditoria(auditoria_dto)
 
-        return Response(map_propiedad.dto_a_externo(dto_final), status=200, mimetype='application/json')
+        return Response(map_auditoria.dto_a_externo(dto_final), status=200, mimetype='application/json')
     except ExcepcionDominio as e:
         return Response(json.dumps({'error': str(e)}), status=400, mimetype='application/json')
 
-@bp.route('/propiedad/<id>', methods=('GET',))
-def obtener_propiedad(id):
+@bp.route('/auditoria/<id>', methods=('GET',))
+def obtener_auditoria(id):
     try:
-        sp = ServicioPropiedad()
-        propiedad_dto = sp.obtener_propiedad_por_id(id)
+        sp = ServicioAuditoria()
+        auditoria_dto = sp.obtener_auditoria_por_id(id)
         
-        if propiedad_dto:
-            map_propiedad = MapeadorPropiedadDTOJson()
-            return Response(map_propiedad.dto_a_externo(propiedad_dto), status=200, mimetype='application/json')
+        if auditoria_dto:
+            map_auditoria = MapeadorAuditoriaDTOJson()
+            return Response(map_auditoria.dto_a_externo(auditoria_dto), status=200, mimetype='application/json')
         else:
             return Response(json.dumps({'error': 'Propiedad no encontrada'}), status=404, mimetype='application/json')
     except ExcepcionDominio as e:
