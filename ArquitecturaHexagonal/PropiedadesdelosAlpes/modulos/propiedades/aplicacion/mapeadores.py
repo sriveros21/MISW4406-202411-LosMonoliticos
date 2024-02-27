6df23aca40a63721ef6793d2497f95dd2c7782d2
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2024-02-27 12:55:50 -0500

    Propiedad Externo a DTO OK

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/aplicacion/mapeadores.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/propiedades/aplicacion/mapeadores.py
@@ -8,37 +8,26 @@ from datetime import datetime
 
 class MapeadorPropiedadDTOJson(AppMap):
     def externo_a_dto(self, propiedad: dict) -> PropiedadDTO:
-        propiedad_dto = PropiedadDTO()  
-        propiedad_dto.nombre=propiedad.nombre
-        propiedad_dto.ubicacion=propiedad.ubicacion
-        propiedad_dto.dimensiones=propiedad.dimensiones
-        propiedad_dto.tipo=propiedad.tipo
-        propiedad_dto.estado=propiedad.estado
-        #Revisar esta linea
-        #propiedad_dto.terreno=propiedad.terreno
-        terreno:TerrenoDTO=TerrenoDTO(
-            id=str(propiedad.terreno.id),
-            dimensiones=propiedad.terreno.dimensiones,
-            lote=propiedad.terreno.lote
+
+        terreno_dto:TerrenoDTO=TerrenoDTO(
+            id=str(propiedad.get('terreno').get('id')),
+            dimensiones=propiedad.get('terreno').get('dimensiones'),
+            lote=propiedad.get('terreno').get('lote')
         )
-        propiedad_dto.terreno=terreno
-
-        #propiedad_dto.terreno.id=propiedad.terreno.id
-        #propiedad_dto.terreno.dimensiones=propiedad.terreno.dimensiones
-        #propiedad_dto.terreno.lote=propiedad.terreno.lote
         
+        edificaciones_dto: List[EdificacionDTO] = [
+            EdificacionDTO(
+                id=str(edificacion.get('id')),
+                tipo=edificacion.get('tipo'),
+                dimensiones=edificacion.get('dimensiones'),
+                pisos=[PisoDTO(descripcion=piso.get('descripcion'), metros_cuadrados=piso.get('metros_cuadrados')) for piso in edificacion.get('pisos')]
+            ) for edificacion in propiedad.get('edificaciones')
+        ]
 
+        propiedad_dto = PropiedadDTO(propiedad.get('nombre'),propiedad.get('ubicacion'),propiedad.get('dimensiones'),
+                                     propiedad.get('tipo'), propiedad.get('estado'), edificaciones=edificaciones_dto,
+                                    terreno=terreno_dto)  
 
-        edificaciones_dto:list[EdificacionDTO]=list()
-
-        for edificacion in propiedad.get('edificaciones',list()):
-            pisos_dto:list[PisoDTO]=list()
-            for piso in edificacion.get('pisos',list()):
-                piso_dto:PisoDTO=PisoDTO(piso.get('descripcion'),piso.get('metros_cuadrados'))
-                pisos_dto.append(piso_dto)
-            edificacion_dto:EdificacionDTO=EdificacionDTO(edificacion.get('id'), edificacion.get('tipo'), edificacion.get('dimensiones'), pisos_dto)
-            edificaciones_dto.append(edificacion_dto)
-        propiedad_dto.edificaciones=edificaciones_dto
         return propiedad_dto
     
     def dto_a_externo(self, dto: PropiedadDTO) -> dict:
