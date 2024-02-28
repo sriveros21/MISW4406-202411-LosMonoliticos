65051d88ce2099a444c0bc88a04d60a40ac9e534
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-02-28 12:26:55 -0500

    Fix ovs for client

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/seedwork/dominio/objetos_valor.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/seedwork/dominio/objetos_valor.py
@@ -1,20 +1,24 @@
+from abc import ABC
 from dataclasses import dataclass
-from abc import ABC, abstractmethod
 from datetime import datetime
+
 
 @dataclass(frozen=True)
 class ObjetoValor:
     pass
 
+
 @dataclass(frozen=True)
 class IdentificadorUnico(ABC, ObjetoValor):
     id: str
+
 
 @dataclass(frozen=True)
 class Dimension:
     width: float
     length: float
     unit: str
+
 
 @dataclass(frozen=True)
 class Direccion(ObjetoValor):
@@ -24,22 +28,42 @@ class Direccion(ObjetoValor):
     estado: str
     codigo_postal: str
 
+
 @dataclass(frozen=True)
 class Ubicacion(ObjetoValor):
     direccion: Direccion
     latitud: float
     longitud: float
 
+
 @dataclass(frozen=True)
 class Precio(ObjetoValor):
     valor: float
     moneda: str
 
+
 @dataclass(frozen=True)
 class Fecha(ObjetoValor):
     fecha: datetime
+
 
 @dataclass(frozen=True)
 class NombreCompleto(ObjetoValor):
     nombre: str
     apellido: str
+
+
+@dataclass(frozen=True)
+class EmailContacto(ObjetoValor):
+    correo: str
+
+
+@dataclass(frozen=True)
+class Identificacion(ObjetoValor):
+    tipo: str
+    numero: str
+
+
+@dataclass(frozen=True)
+class FechaRegistro(ObjetoValor):
+    fecha: datetime
