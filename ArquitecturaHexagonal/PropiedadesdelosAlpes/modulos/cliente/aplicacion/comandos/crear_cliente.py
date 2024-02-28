4ee792431ae237aa7f5550ab9622397a22c60449
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-02-28 17:29:19 -0500

    Fixes for client

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/cliente/aplicacion/comandos/crear_cliente.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/cliente/aplicacion/comandos/crear_cliente.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
-from PropiedadesdelosAlpes.modulos.cliente.aplicacion.dto import ClienteDTO
+from PropiedadesdelosAlpes.modulos.cliente.aplicacion.dto import NombreDTO, ApellidoDTO, EmailDTO, ClienteDTO
+from PropiedadesdelosAlpes.modulos.cliente.aplicacion.mapeadores import MapeadorCliente
 from PropiedadesdelosAlpes.modulos.cliente.dominio.entidades import Cliente
+from PropiedadesdelosAlpes.modulos.cliente.infraestructura.repositorios import RepositorioCliente
 from PropiedadesdelosAlpes.seedwork.aplicacion.comandos import Comando
 from PropiedadesdelosAlpes.seedwork.aplicacion.comandos import ejecutar_commando as comando
 from PropiedadesdelosAlpes.seedwork.infraestructura.uow import UnidadTrabajoPuerto
@@ -11,21 +13,26 @@ from .base import CrearClienteBaseHandler
 
 @dataclass
 class CrearCliente(Comando):
-    nombre: str
-    apellido: str
+    nombre: NombreDTO
+    apellido: ApellidoDTO
+    email: EmailDTO
 
 
 class CrearClienteHandler(CrearClienteBaseHandler):
     def handle(self, comando: CrearCliente):
         cliente_dto = ClienteDTO(
             nombre=comando.nombre,
-            apellido=comando.apellido
+            apellido=comando.apellido,
+            email=comando.email
         )
 
-        cliente: Cliente = self.fabrica_cliente.crear_desde_dto(cliente_dto)
-        repositorio = self.fabrica_repositorio.obtener_repositorio_cliente()
+        cliente: Cliente = self.fabrica_cliente.crear_objeto(cliente_dto, MapeadorCliente())
+        cliente.crear_cliente(cliente)
 
-        UnidadTrabajoPuerto.registrar_batch(repositorio.agregar(), cliente)
+        repositorio = self.fabrica_repositorio.crear_objeto(RepositorioCliente.__class__)
+
+        UnidadTrabajoPuerto.registrar_batch(repositorio.agregar, cliente)
+        UnidadTrabajoPuerto.savepoint()
         UnidadTrabajoPuerto.commit()
 
 
