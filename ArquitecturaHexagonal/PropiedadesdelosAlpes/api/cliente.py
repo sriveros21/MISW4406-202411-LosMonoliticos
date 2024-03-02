80be449626b605b1de410e2758bc5f24e85d2a1f
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-03-02 17:32:43 -0500

    Fix Cliente

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/api/cliente.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/api/cliente.py
@@ -20,23 +20,25 @@ def crear_cliente():
         mapeador = current_app.extensions['mapeador_cliente']
         sc = ServicioCliente(repositorio=repositorio, mapeador=mapeador)
         dto_final = sc.crear_cliente(cliente_dto)
+        print(dto_final)  # Print the value of dto_final
 
-        return Response(map_cliente.dto_a_externo(dto_final), status=200, mimetype='application/json')
+        return Response(json.dumps(map_cliente.dto_a_externo(dto_final)), status=200, mimetype='application/json')
     except ExcepcionDominio as e:
         return Response(json.dumps({'error': str(e)}), status=400, mimetype='application/json')
 
 
-@bp.route('/cliente/<id>', methods=('GET',))
-def obtener_cliente(id):
+@bp.route('/cliente/<id_cliente>', methods=('GET',))
+def obtener_cliente(id_cliente):
     try:
         repositorio = current_app.extensions['repositorio_cliente']
         mapeador = current_app.extensions['mapeador_cliente']
         sc = ServicioCliente(repositorio=repositorio, mapeador=mapeador)
-        cliente_dto = sc.obtener_cliente_por_id(id)
+        cliente_dto = sc.obtener_cliente_por_id(id_cliente)
+        print("CLIENTE DTO", cliente_dto)  # Print the value of cliente_dto
 
         if cliente_dto:
             map_cliente = MapeadorClienteDTOJson()
-            return Response(map_cliente.dto_a_externo(cliente_dto), status=200, mimetype='application/json')
+            return Response(json.dumps(map_cliente.dto_a_externo(cliente_dto)), status=200, mimetype='application/json')
         else:
             return Response(json.dumps({'error': 'Cliente no encontrado'}), status=404, mimetype='application/json')
     except ExcepcionDominio as e:
