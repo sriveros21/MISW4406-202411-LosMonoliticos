2dcf4fd27f28e7a655cb48f090782ec9f253c976
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-03-04 19:37:28 -0500

    Fix Cliente Pulsar5

--- ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/cliente/infraestructura/mapeadores.py
+++ ArquitecturaHexagonal/PropiedadesdelosAlpes/modulos/cliente/infraestructura/mapeadores.py
@@ -1,4 +1,4 @@
-from PropiedadesdelosAlpes.modulos.cliente.dominio.entidades import Cliente as ClienteEntidad
+from PropiedadesdelosAlpes.modulos.cliente.dominio.entidades import Cliente
 from PropiedadesdelosAlpes.modulos.cliente.dominio.objetos_valor import Nombre, Apellido, Email
 from PropiedadesdelosAlpes.seedwork.dominio.repositorios import Mapeador
 
@@ -9,9 +9,9 @@ from ..dominio.objetos_valor import Apellido
 class MapeadorCliente(Mapeador):
 
     def obtener_tipo(self) -> type:
-        return ClienteEntidad.__class__
+        return Cliente.__class__
 
-    def entidad_a_dto(self, entidad: ClienteEntidad) -> ClienteDTO:
+    def entidad_a_dto(self, entidad: Cliente) -> ClienteDTO:
         cliente_dto = ClienteDTO()
         cliente_dto.id_cliente = ''.join(map(str, entidad.id_cliente))
         cliente_dto.nombre = entidad.nombre
@@ -19,13 +19,13 @@ class MapeadorCliente(Mapeador):
         cliente_dto.email = entidad.email
         return cliente_dto
 
-    def dto_a_entidad(self, dto: ClienteDTO) -> ClienteEntidad:
+    def dto_a_entidad(self, dto: ClienteDTO) -> Cliente:
         id_cliente = dto.id_cliente
         nombre = Nombre(nombre=dto.nombre)
         apellido = Apellido(apellido=dto.apellido)
         email = Email(email=dto.email)
 
-        return ClienteEntidad(
+        return Cliente(
             id_cliente=id_cliente,
             nombre=nombre,
             apellido=apellido,
